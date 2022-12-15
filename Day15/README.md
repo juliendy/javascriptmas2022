6f9e6002b05b5da309d7641982aede895c0dd312
juliendy <julien.yasar@gmail.com>
2022-12-15 04:31:29 +0100

    feat: day15 finished. some edgecases needs to be sorted + styling.

---
+++ Day15/README.md
@@ -0,0 +1,12 @@
+# ↔️ Palindromes
+
+## The challange:
+Palindromes are words that are the same forward or backward. For example, 
+the words "noon" and "kayak" are a palindromes.
+ 
+Write a function to check if a lowercased string of letters is a palindrome. 
+If the word is palidrome, return true. If it isn't, return false. 
+
+This version of the code is using my own, to suit it for a one page application where
+the user can input a word and it is check if it is a palindrome. Two see the two versions,
+check the root repository readme for links.
---
+++ Day15/app.js
@@ -0,0 +1,63 @@
+// get the form and the result paragraph
+const form = document.querySelector("form");
+const result = document.querySelector("#result");
+
+// function to check if a word is a palindrome
+function isPalindrome(str) {
+    let i = 0;
+    let j = str.length - 1;
+
+    while (i < j) {
+        // check if the characters at the current positions match, ignoring case
+        if (str[i].toLowerCase() !== str[j].toLowerCase()) {
+            return false;
+        }
+
+        i++;
+        j--;
+    }
+
+    return true;
+}
+
+// function to reverse a word
+function reverseWord(word) {
+    let reversed = "";
+    for (let i = word.length - 1; i >= 0; i--) {
+        // check if the current character is uppercase
+        if (word[i] === word[i].toUpperCase()) {
+            // if it is, make the corresponding character in the reversed word uppercase too
+            reversed += word[i].toUpperCase();
+        } else {
+            // if it's not, make the corresponding character in the reversed word lowercase
+            reversed += word[i].toLowerCase();
+        }
+    }
+    return reversed;
+}
+
+// listen for submit events on the form
+form.addEventListener("submit", (event) => {
+    // prevent the form from actually submitting
+    event.preventDefault();
+
+    // get the word from the input field
+    const word = document.querySelector("#word").value;
+
+    // reverse the word
+    let reversed = reverseWord(word);
+
+    // check if the first character of the original word was uppercase
+    if (word[0] === word[0].toUpperCase()) {
+        // If it was, make the first character of the reversed word uppercase too
+        reversed = reversed[0].toUpperCase() + reversed.substring(1);
+    }
+
+    // check if the original word is a palindrome
+    const isWordPalindrome = isPalindrome(word);
+
+    // show the result
+    result.textContent = `The word "${word}" backwards is "${reversed}". ${
+        isWordPalindrome ? "It is a palindrome!" : "It is not a palindrome."
+    }`;
+});
